595df5dfdb1557020aebea4a8086d0c379908c74
MagicSale <75734817+matteosalis04@users.noreply.github.com>
2025-05-03 08:39:55 +0200

    Add files via upload

---
+++ Linux/auditing.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+###########################################
+######## Script Auditing ##################
+###########################################
+############### by magicsale ##############
+###########################################
+##################################
+# GitHub: https://github.com/matteosalis04
+##################################
+
+
+data=$(date +"%d-%m-%Y %H:%M:%S")
+
+
+if [[ $EUID != 0 ]]; then
+    echo "please run as root"
+    exit 0
+fi
+
+file_log="/root/audit_log.log"
+
+if [[ ! -f "$file_log" ]]; then
+    touch /root/audit_logs.log
+fi
+cat << "EOF"
+
+****************************************
+        AUDIT LOG - Bash, Linux
+                    v.1.0
+****************************************
+
+EOF
+read -p "Key: " key
+
+
+
+# Estrai eventi di accesso negati
+ausearch -i -k $key | grep "success=no" | while read line; do
+    # Estrai il nome utente, orario, command e il percorso dell'eseguibile
+    user=$(echo "$line" | awk -F 'euid=' '{print $2}' | awk '{print $1}')
+    timestamp=$(echo "$line" | grep -oP '\bmsg=audit\(\K[^\)]+')
+    command=$(echo "$line" | awk -F 'comm=' '{print $2}' | awk '{print $1}')
+    exe=$(echo "$line" | awk -F 'exe=' '{print $2}' | awk '{print $1}')
+    # possibile modifica ai file con echo  
+    if [[ $command == "bash" ]]; then
+        command=$(echo "Bash - possibile modifica file tramite shell (echo, etc) !!!!!!")
+    fi
+
+    echo "Utente: $user"
+    echo "Comando: $command"
+    echo "Esecuzione: $exe"
+    echo "Data: $timestamp"
+    echo "**************************************"
+    echo ""
+done | tee -a $file_log
+
+
+echo "In allegato trovi il file di log con il report delle attività di auditing del sistema." > message.dat
+mpack -s "Report Audit - $data" -d "./message.dat" $file_log matteosalis04@gmail.com
+echo "Email Inviata!!!"
+
+rm -rf $file_log
+
+
+
+
---
+++ Windows/auditing.ps1
@@ -0,0 +1,60 @@
+# Audit System Powershell
+###########################################
+######## Script Auditing ##################
+###########################################
+############### by magicsale ##############
+###########################################
+##################################
+# GitHub: https://github.com/matteosalis04
+##################################
+$data = Get-Date
+$log = "C:\log_audit.txt"
+$validIDs = @("4776", "4624", "4720", "4732", "4733", "4726", "1102", "4740", "4647", "4656", "4663", "4658")
+
+function help() {
+Write-Host "Event ID:"
+@"
+4776: Fail Logon.
+4624: An account was successfully logged on.
+4720: A user account was created.
+4732: A member was added from a security-enabled local group.
+4733: A member was added from a security-enabled local group.
+4726: A user account was deleted
+1102: the audit log was cleared
+4740: A user account was locked out
+4647: user initiated logoff
+4656: An Handle to an object was requested.
+4663: An attempt was made to access an object
+4658: the handle of an object was closed
+"@ | Write-Host
+return
+}
+function audit($idname, $userdata) {
+    if ([string]::IsNullOrEmpty($userdata)) {
+        Get-WinEvent -FilterHashtable @{LogName='Security'; Id=$idname} | Sort-Object TimeCreated -Descending | Format-List
+    } else {
+        Get-WinEvent -FilterHashtable @{LogName='Security'; Id=$idname; Data=$userdata} | Sort-Object TimeCreated -Descending | Format-List
+    }}
+
+
+function main() {
+    Write-Host "Welcome to Audit - Powershell | Windows"
+    Write-Host "-----------------------------------------"
+    Write-Host "-------------------     $data - By MS    "
+    $ID = Read-Host("Scrivi ID che vuoi monitorare (o 'help' per aiuto)")
+    if ( $ID -eq "help" ) {
+        help
+        exit 1
+    }
+
+    if ($validIDs -contains $ID) {
+        $target = Read-Host("Scrivi (user, handle ID, Type) che vuoi monitorare (o lascia vuoto)")
+        audit $ID $target | Tee-Object -FilePath $log
+    } else {
+        Write-Host "Comando non valido!"
+        exit 1
+    }
+}
+
+main 
+
